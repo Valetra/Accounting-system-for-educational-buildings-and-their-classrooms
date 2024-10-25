ec9add8ed331ef00ada5465f86e881cd4099a6c3
valetra <valetra100102@gmail.com>
2024-10-25 10:42:47 +0300

    add mapper

---
+++ BuildingAPI/Mapper/AppMappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace Mapper;
+
+public class AppMappingProfile : Profile
+{
+	public AppMappingProfile()
+	{
+		CreateMap<DAL.Models.Building, ResponseObjects.Building>();
+		CreateMap<RequestObjects.Building, DAL.Models.Building>();
+	}
+}

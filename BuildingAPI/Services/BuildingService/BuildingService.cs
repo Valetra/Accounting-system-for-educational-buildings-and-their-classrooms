535c934e94d3ec25c1cda07559ade0873fe46f41
valetra <valetra100102@gmail.com>
2024-10-30 14:12:32 +0300

    fix bug

--- BuildingAPI/Services/BuildingService/BuildingService.cs
+++ BuildingAPI/Services/BuildingService/BuildingService.cs
@@ -3,15 +3,15 @@ using DAL.Repository;
 
 namespace Service;
 
-public class BuildingService(BuildingRepository studyBuildingRepository) : IBuildingService
+public class BuildingService(IBuildingRepository buildingRepository) : IBuildingService
 {
-	public Task<List<Building>> GetAll() => Task.FromResult(studyBuildingRepository.GetAll().ToList());
+	public Task<List<Building>> GetAll() => Task.FromResult(buildingRepository.GetAll().ToList());
 
-	public Task<Building?> Get(Guid id) => studyBuildingRepository.Get(id);
+	public Task<Building?> Get(Guid id) => buildingRepository.Get(id);
 
-	public Task<Building> Create(Building model) => studyBuildingRepository.Create(model);
+	public Task<Building> Create(Building model) => buildingRepository.Create(model);
 
-	public Task<Building?> Update(Building model) => studyBuildingRepository.Update(model);
+	public Task<Building?> Update(Building model) => buildingRepository.Update(model);
 
-	public async Task<bool> Delete(Guid id) => await studyBuildingRepository.Delete(id);
+	public async Task<bool> Delete(Guid id) => await buildingRepository.Delete(id);
 }

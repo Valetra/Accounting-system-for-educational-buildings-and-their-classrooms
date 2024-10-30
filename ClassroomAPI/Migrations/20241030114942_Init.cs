32bdc6bb7eef26da28817a8a388fc74b1cb2e96d
valetra <valetra100102@gmail.com>
2024-10-30 15:40:27 +0300

    add migration

---
+++ ClassroomAPI/Migrations/20241030114942_Init.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassroomAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classrooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BuildingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    Floor = table.Column<int>(type: "integer", nullable: false),
+                    Number = table.Column<byte>(type: "smallint", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classrooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShortBuildingInfo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortBuildingInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classrooms_Name",
+                table: "Classrooms",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortBuildingInfo_Address",
+                table: "ShortBuildingInfo",
+                column: "Address");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortBuildingInfo_Name",
+                table: "ShortBuildingInfo",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classrooms");
+
+            migrationBuilder.DropTable(
+                name: "ShortBuildingInfo");
+        }
+    }
+}

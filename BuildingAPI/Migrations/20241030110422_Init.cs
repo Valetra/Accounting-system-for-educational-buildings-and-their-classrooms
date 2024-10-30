a379a232550024bf60d54556c3b2a8c95b7d0eda
valetra <valetra100102@gmail.com>
2024-10-30 14:14:16 +0300

    add migration

---
+++ BuildingAPI/Migrations/20241030110422_Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    FloorsCount = table.Column<byte>(type: "smallint", nullable: false),
+                    Other = table.Column<string>(type: "text", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_Address",
+                table: "Buildings",
+                column: "Address");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_Name",
+                table: "Buildings",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Buildings");
+        }
+    }
+}
